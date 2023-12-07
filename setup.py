f79c37375549d378213b41f95d3cefd9f6bf6c76
Sameersam100 <mohammedabduls674@gmail.com>
2023-12-07 19:47:40 +0300

    Project-4

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages=["zomato_analysis"],
     package_dir={"zomato_analysis": "src/zomato_analysis"},
     description="A simple package for Zomato restaurants data analysis",
-    author="Sameer",
-    author_email="your.email@example.com",
+    author="Abdul Sameer Mohammed",
+    author_email="amohamm8@mail.yu.edu",
     keywords=["zomato", "data analysis", "predictive modeling"],
-    url="https://github.com/yourusername/your_package_repo",
+    url="https://github.com/Sameersam100/Predictive-Modeling/",
     install_requires=[
         "pandas",
         "numpy",
